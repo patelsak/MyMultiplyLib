3ee18b0b8a86a9ac4e81aaf5ed6bb7c03d6686fd
patelsak <74833044+patelsak@users.noreply.github.com>
2022-02-11 08:52:03 -0700

    Add files via upload

---
+++ MyMultiplyLib/Multiply.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMultiplyLib;
+public class Multiply
+{
+    public static int Multi(int first_num, int second_num){
+        return first_num * second_num;
+    }
+    public static int Multi(int first_num, int second_num, int third_num){
+        return first_num * second_num * third_num;  
+    }
+}
